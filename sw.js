81e2fe2cf500000d6a5d18e707b1ad6879bfeebe
t==Bz&\Mh+r=:ChfA0;! <ziadelwali@gmail.com>
2026-09-11 21:25:37 +0200

    Rotate passphrase

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell cache. Holds only the encrypted file; nothing decrypted
    is ever written to storage. Rebuilt on every publish. */
-const VERSION = 'v-8d470ba0b9';
+const VERSION = 'v-b57867ad19';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-180.png'];
 
 self.addEventListener('install', function (e) {
