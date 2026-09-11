105da0b94b351c3f2f24ac6daacb023f4cc29358
t==Bz&\Mh+r=:ChfA0;! <ziadelwali@gmail.com>
2026-09-11 23:04:56 +0200

    Theme toggle on mobile, no forced dark

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell cache. Holds only the encrypted file; nothing decrypted
    is ever written to storage. Rebuilt on every publish. */
-const VERSION = 'v-f582cce93a';
+const VERSION = 'v-9545275045';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-180.png'];
 
 self.addEventListener('install', function (e) {
